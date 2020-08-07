3294dac6b5bcd5ddcae8c53649de6c10c2825e8a
Baltej Parmar <bparmar@uwaterloo.ca>
2020-08-07 01:20:34 +0000

    Only users can see their own shortened urls and only logged in users can edit or delete

--- express_server.js
+++ express_server.js
@@ -19,13 +19,8 @@ function generateRandomString() {
 
 app.set("view engine", "ejs");
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
-
 const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "userRandomID" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
@@ -42,24 +37,38 @@ const users = {
   }
 }
 
+const urlsForUser = function(database, id) {
+  newDatabase = {};
+  for (let user in database) {
+    console.log(user);
+    if (database[user].userID === id) {
+      newDatabase[user] = database[user];
+    }
+  }
+  return newDatabase;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//urlsForUser(urlDatabase,users[req.cookies["user_id"].id]
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase,
+  // let templateVars = { urls: urlDatabase,
+  //   user: users[req.cookies["user_id"]],
+  // };
+  let templateVars = { urls: urlsForUser(urlDatabase,req.cookies["user_id"]),
     user: users[req.cookies["user_id"]],
-    // ... any other vars
   };
-  //console.log(templateVars);
+  //console.log(req.cookies["user_id"]);
+  //console.log(templateVars.urls);
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  //console.log(req.body);  // Log the POST request body to the console
   let templateVars = { urls: urlDatabase,
     user: users[req.cookies["user_id"]],
-    // ... any other vars
   };
   const newShortform = generateRandomString();
   urlDatabase[newShortform] = req.body["longURL"];
@@ -69,14 +78,12 @@ app.post("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: urlDatabase,
     user: users[req.cookies["user_id"]],
-    // ... any other vars
   };
   if (templateVars.user) {
     res.render("urls_new",templateVars);
   } else {
     res.redirect("/login")
   }
-  //res.render("urls_new",templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -90,7 +97,16 @@ app.get("/hello", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, 
     user: users[req.cookies["user_id"]]};
-  res.render("urls_show", templateVars); 
+    console.log(req.cookies["user_id"]);
+    console.log(urlDatabase[req.params.shortURL].userID);
+
+    if (!req.cookies["user_id"]) {
+      res.status(400).send("Error: User is not logged in. Please login to continue");
+    } else if (req.cookies["user_id"] !== urlDatabase[req.params.shortURL].userID) {
+      res.status(400).send("Error: You do not have access to this URL");
+    } else {
+      res.render("urls_show", templateVars); 
+    }
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -107,13 +123,23 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
+  if (!req.cookies["user_id"]) {
+    res.status(403).send("Cannot edit or delete URL if you are not logged in")
+  } else {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+  }
+  // delete urlDatabase[req.params.shortURL];
+  // res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL", (req,res) => {
-  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
-  res.redirect("/urls")
+  if (!req.cookies["user_id"]) {
+    res.status(403).send("Cannot edit URL if you are not logged in")
+  } else {
+    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+    res.redirect("/urls")
+  }
 })
 
 app.post("/login", (req,res) => {
@@ -125,8 +151,6 @@ app.post("/login", (req,res) => {
         if (users[id].password !== req.body.password) {
           res.status(403).send("Error 403: Password doesn't match email")
         } else {
-          //res.redirect("/urls")
-         // console.log(req.body.email);
          const fullUser = userIDChecker(req.body.email,users)
           res.cookie("user_id",fullUser.id)
           res.redirect("/urls")
@@ -137,9 +161,6 @@ app.post("/login", (req,res) => {
 })
 
 app.post("/logout", (req,res) => {
-  //res.clearCookie("username")
-  //console.log("hi");
-  //console.log(req.cookies.user_id);
   res.cookie("user_id",null);
   res.redirect("/urls")
 })
